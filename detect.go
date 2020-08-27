471b8d5fd446dc252bd5948c84b3a6a700727ff7
Alan D. Salewski <salewski@att.net>
2020-08-27 17:34:48 -0400

    [1 of n] getter.Detect(...): extract function handleDetected(...)
    Once the Detect(...) function finds a detector for a source string it combines
    the obtained result string with other available data to produce the result
    returned to the caller. That processing is here extracted into a new
    handleDetected(...) function, which will be called from an additional context
    in an upcoming commit.
    
    This changeset provides an implementation modification, but no behavioral
    change.

--- detect.go
+++ detect.go
@@ -61,45 +61,54 @@ func Detect(src string, pwd string, ds []Detector) (string, error) {
 			continue
 		}
 
-		var detectForce string
-		detectForce, result = getForcedGetter(result)
-		result, detectSubdir := SourceDirSubdir(result)
-
-		// If we have a subdir from the detection, then prepend it to our
-		// requested subdir.
-		if detectSubdir != "" {
-			if subDir != "" {
-				subDir = filepath.Join(detectSubdir, subDir)
-			} else {
-				subDir = detectSubdir
-			}
-		}
-
-		if subDir != "" {
-			u, err := url.Parse(result)
-			if err != nil {
-				return "", fmt.Errorf("Error parsing URL: %s", err)
-			}
-			u.Path += "//" + subDir
-
-			// a subdir may contain wildcards, but in order to support them we
-			// have to ensure the path isn't escaped.
-			u.RawPath = u.Path
-
-			result = u.String()
-		}
-
-		// Preserve the forced getter if it exists. We try to use the
-		// original set force first, followed by any force set by the
-		// detector.
-		if getForce != "" {
-			result = fmt.Sprintf("%s::%s", getForce, result)
-		} else if detectForce != "" {
-			result = fmt.Sprintf("%s::%s", detectForce, result)
+		result, err = handleDetected(result, getForce, subDir)
+		if err != nil {
+			return "", err
 		}
 
 		return result, nil
 	}
 
 	return "", fmt.Errorf("invalid source string: %s", src)
+}
+
+func handleDetected(detectedResult, srcGetForce, subDir string) (string, error) {
+	var detectForce string
+	detectForce, result := getForcedGetter(detectedResult)
+	result, detectSubdir := SourceDirSubdir(result)
+
+	// If we have a subdir from the detection, then prepend it to our
+	// requested subdir.
+	if detectSubdir != "" {
+		if subDir != "" {
+			subDir = filepath.Join(detectSubdir, subDir)
+		} else {
+			subDir = detectSubdir
+		}
+	}
+
+	if subDir != "" {
+		u, err := url.Parse(result)
+		if err != nil {
+			return "", fmt.Errorf("Error parsing URL: %s", err)
+		}
+		u.Path += "//" + subDir
+
+		// a subdir may contain wildcards, but in order to support them we
+		// have to ensure the path isn't escaped.
+		u.RawPath = u.Path
+
+		result = u.String()
+	}
+
+	// Preserve the forced getter if it exists. We try to use the
+	// original set force first, followed by any force set by the
+	// detector.
+	if srcGetForce != "" {
+		result = fmt.Sprintf("%s::%s", srcGetForce, result)
+	} else if detectForce != "" {
+		result = fmt.Sprintf("%s::%s", detectForce, result)
+	}
+
+	return result, nil
 }
